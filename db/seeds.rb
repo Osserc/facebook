4dfc97cf9eac88e577c74ed89d6234ffe0f74328
Stefano Rondelli <rstefanofil@hotmail.com>
2022-05-25 14:36:23 +0200

    Create User model with devise and prepare seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+first_names = ["Steven", "Mack", "Joanna", "Lot", "Frederick", "Sieglinde", "Oliver", "Winston", "Charles", "Victoria"]
+last_names = ["Locking", "Cortas", "Vilnius", "Bet-Beniamin", "Collins", "West", "Crom", "Jill", "David", "Marsh"]
+emails = ["user1@proton.com", "user2@proton.com", "user3@proton.com", "user4@proton.com", "user5@proton.com", "user6@proton.com", "user7@proton.com", "user8@proton.com", "user9@proton.com", "user10@proton.com"]
+
+i = 0
+emails.each do | mail |
+    User.create(first_name: first_names[i], last_name: last_names[i], email: mail, password: "123456")
+    i += 1
+end
