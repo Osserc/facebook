168a74390473e2810080def7318913187b4be8cf
Stefano Rondelli <rstefanofil@hotmail.com>
2022-05-27 13:57:24 +0200

    Add Friend relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,14 @@ class User < ApplicationRecord
   has_many :received_requests, class_name: "FriendRequest", foreign_key: :receiver_id
   has_many :wannabe_friends, through: :received_requests, source: :sender
 
+  has_many :initiated_friendhips, class_name: "Friendship", foreign_key: :friend_one_id
+  has_many :befriendeds, through: :initiated_friendhips, source: :friend_two
+
+  has_many :received_friendships, class_name: "Friendship", foreign_key: :friend_two_id
+  has_many :befriended_bys, through: :received_friendships, source: :friend_one
+
+  def friends
+    return self.befriendeds + self.befriended_bys
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_27_073926) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_27_115134) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_27_073926) do
   create_table "friend_requests", force: :cascade do |t|
     t.integer "sender_id"
     t.integer "receiver_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "friend_one_id"
+    t.integer "friend_two_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
