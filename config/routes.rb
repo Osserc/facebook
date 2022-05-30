b9764d53a8af66c88671a6fc387365f229110254
Stefano Rondelli <rstefanofil@hotmail.com>
2022-05-30 18:30:34 +0200

    Nest comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: %i[ index show ]  do
-    resources :posts
+    resources :posts do
+      resources :comments
+    end
+  end
+
+  resources :comments do
+    resources :comments
   end
 
   resources :friend_requests, :friendships, :followings, :blockings, :likes, only: %i[ create destroy ]
   resources :comments, except: %i[ index show ]
-  # post '/friend_requests/send_request', to: "friend_requests#send_requests", as: "register"
-  # post '/friend_requests/unregister', to: "participations#unregister", as: "unregister"
-  # post '/favorites/add_to_favorites', to: "favorites#add_to_favorites", as: "add_to_favorites"
-  # post '/favorites/remove_from_favorites', to: "favorites#remove_from_favorites", as: "remove_from_favorites"
-  # post '/invitations/generate_invite_list', to: "invitations#generate_invite_list", as: "generate_invite_list"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
