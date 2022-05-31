68f7aa8072cb6a4d1d7145798c99eb3d35ed040d
Stefano Rondelli <rstefanofil@hotmail.com>
2022-05-31 17:44:43 +0200

    Add turbo functionality to post likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,30 @@
 class LikesController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_turbo_object
 
     def create
         current_user.likes.create(like_params)
+        respond_to do |format|
+            format.turbo_stream { flash.now[:notice] = "Liked post." }
+        end
     end
 
     def destroy
         helpers.find_like(params[:likeable_type], params[:likeable_id]).destroy
+        respond_to do |format|
+            format.turbo_stream { flash.now[:notice] = "Unliked post." }
+        end
     end
 
     private
     def like_params
         params.permit(:likeable_type, :likeable_id)
+    end
+
+    def set_turbo_object
+        @object = "like_" + params[:likeable_type].downcase + "_" + params[:likeable_id]
+        @type = params[:likeable_type]
+        @id = params[:likeable_id]
     end
 
 end
