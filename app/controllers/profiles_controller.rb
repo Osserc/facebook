e1bb60e04ec56b3f727299563974bf2a8494c996
Stefano Rondelli <rstefanofil@hotmail.com>
2022-06-03 23:09:13 +0200

    Add avatar uploading feature

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,12 +6,21 @@ class ProfilesController < ApplicationController
     end
 
     def update
+        if @profile.update!(profile_params)
+            redirect_to user_path(@user)
+        else
+            render :edit, status: :unprocessable_entity
+        end
     end
 
     private
     def set_user_and_profile
         @user = User.find(params[:user_id])
         @profile = @user.profile
+    end
+
+    def profile_params
+        params.require(:profile).permit(:bio, :avatar)
     end
 
 end
