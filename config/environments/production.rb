53bdba5552dd4df62d3dc05fd271a7d102da0224
Stefano Rondelli <rstefanofil@hotmail.com>
2022-06-05 17:42:57 +0200

    Add gmail account for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "facebook_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  host = 'example.com' #replace with your own url
+  config.action_mailer.default_url_options = { host: host }
+
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV["gmail_rails_username"],
+    :password             => ENV["gmail_rails_password"],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
