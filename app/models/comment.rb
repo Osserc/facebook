cb439b48d300730822b60e780ed89ed04e6d318b
Stefano Rondelli <rstefanofil@hotmail.com>
2022-06-19 18:32:56 +0200

    Extend .css to followings and likers

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
     belongs_to :commentable, polymorphic: true
     belongs_to :author, class_name: "User", foreign_key: "author_id"
     belongs_to :root, class_name: "Post"
-    has_many :comments, as: :commentable
+    has_many :comments, as: :commentable, dependent: :destroy
     has_many :likes, as: :likeable
     has_one :notification, as: :notifiable
 
