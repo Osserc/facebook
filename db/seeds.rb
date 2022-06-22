a55b8f6b33e3bedd3634064feff0d7b5147ad93d
Stefano Rondelli <rstefanofil@hotmail.com>
2022-06-22 18:05:21 +0200

    Remove seeded bios, fix (again) signup/signin images

--- db/seeds.rb
+++ db/seeds.rb
@@ -65,14 +65,11 @@ end
 first_names = ["Steven", "Mack", "Joanna", "Lot", "Frederick", "Sieglinde", "Oliver", "Winston", "Charles", "Victoria"]
 last_names = ["Locking", "Cortas", "Vilnius", "Bet-Beniamin", "Collins", "West", "Crom", "Jill", "David", "Marsh"]
 emails = ["user1@proton.com", "user2@proton.com", "user3@proton.com", "user4@proton.com", "user5@proton.com", "user6@proton.com", "user7@proton.com", "user8@proton.com", "user9@proton.com", "user10@proton.com"]
-bios = ["I abandoned civilization. Forever.", "", "", "Don't stick your nose where it doesn't belong", "", "Hello there, pipsqueak!", "", "", "Mind your manners!", ""]
 
 users = Array.new
 i = 0
 emails.each do | mail |
-    user = User.create(first_name: first_names[i], last_name: last_names[i], email: mail, password: "123456")
-    user.profile.bio = bios[i]
-    users << user
+    users << User.create(first_name: first_names[i], last_name: last_names[i], email: mail, password: "123456")
     i += 1
 end
 
