7279100c89912f81acab11478af2738cdffa114a
Stefano Rondelli <rstefanofil@hotmail.com>
2022-06-22 18:28:13 +0200

    Updated production mailing host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  host = 'example.com' #replace with your own url
+  host = "monkebook.herokuapp.com"
   config.action_mailer.default_url_options = { host: host }
 
   config.action_mailer.smtp_settings = {
