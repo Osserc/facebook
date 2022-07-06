2d0f56d295635d86132a455633455e67266874a0
Stefano Rondelli <rstefanofil@hotmail.com>
2022-07-06 14:17:51 +0200

    Add stimulus controller to swap buttons in edit#post

---
+++ app/javascript/controllers/button_swapping_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="button-swapping"
+export default class extends Controller {
+  static targets = [ "editButton", "editForm" ]
+
+  connect() {
+    console.log("Hi!")
+  }
+
+  swap() {
+    this.editFormTarget.action = `${this.editButtonTarget.dataset.value.slice(0, -5)}`;
+    this.editButtonTarget.dataset.value = `${this.editButtonTarget.dataset.value.slice(0, -5)}`;
+    this.editFormTarget.dataset.action = "turbo:frame-load@window->button-swapping#unswap"
+    this.editButtonTarget.innerHTML = "Close"
+  }
+
+  unswap() {
+    this.editFormTarget.action = `${this.editButtonTarget.dataset.value}` + `/edit`;
+    this.editButtonTarget.dataset.value = `${this.editButtonTarget.dataset.value}` + `/edit`;
+    this.editFormTarget.dataset.action = "turbo:frame-load@window->button-swapping#swap";
+    this.editButtonTarget.innerHTML = "Edit"
+  }
+}
